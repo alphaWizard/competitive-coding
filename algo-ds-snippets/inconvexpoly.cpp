30093b1e3d8737510f05e9d9d1f17cad56344143
Arpit Gupta <alphaWizard@users.noreply.github.com>
2018-03-11 16:49:58 +0530

    added on segment,intersection and poly within poly
    added on segment,intersection and poly within poly cpnvex criteria

--- algo-ds-snippets/inconvexpoly.cpp
+++ algo-ds-snippets/inconvexpoly.cpp
@@ -53,3 +53,64 @@ LL r5 = areaTriangle(C[0], C[N - 1], p);
  return (r0 == (r1 + r2 + r3) && r3 != 0 && r4 != 0 && r5 != 0);
 }
 
+
+
+
+// To find orientation of ordered triplet (p, q, r).
+// The function returns following values
+// 0 --> p, q and r are colinear
+// 1 --> Clockwise
+// 2 --> Counterclockwise
+int orientation(Point p, Point q, Point r)
+{
+    int val = (q.y - p.y) * (r.x - q.x) -
+        (q.x - p.x) * (r.y - q.y);
+
+    if (val == 0) return 0;  // colinear
+    return (val > 0)? 1: 2; // clock or counterclock wise
+}
+bool doInstersect(Point p1,Point p2,Point p3,Point p4)
+{
+    int d1,d2,d3,d4;
+    d1=orientation(p3,p4,p1);
+    d2=orientation(p3,p4,p2);
+    d3=orientation(p1,p2,p3);
+    d4=orientation(p1,p2,p4);
+    if(((d1==1&&d2==2)||(d1==2&&d2==1))&&((d3==1&&d4==2)||(d3==2&&d4==1)))
+        return true;
+    else if(d1==0 && onSegment(p3,p4,p1))
+        return true;
+    else if(d2==0 && onSegment(p3,p4,p2))
+        return true;
+    else if(d3==0 && onSegment(p1,p2,p3))
+        return true;
+    else if(d4==0 && onSegment(p1,p2,p4))
+        return true;
+    else 
+        return false;
+}
+bool onSegment(Point p1, Point p2, Point p3)
+{
+    if(p3.x>=(min(p1.x,p2.x))&&(p3.x<=max(p1.x,p2.x))&&(p3.y>=min(p1.y,p2.y))&&(p3.y<=max(p1.y,p2.y)))
+    {
+        return true;
+    }
+    return false;
+}
+bool isInside(Point inner[MAX], int n, Point outer[MAX], int m)
+{
+    bool isInside=true;
+    int prev_orientation,curr_orientation;
+    for(int i=0;i<n;i++)
+    {
+        prev_orientation=orientation(inner[i],outer[0],outer[1]);
+        for(int j=0;j<m;j++)
+        {
+            curr_orientation=orientation(inner[i],outer[j],outer[(j+1)%m]);
+            if(curr_orientation!=prev_orientation)
+                return false;
+        }
+    }
+    return true;
+}
+
